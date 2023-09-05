43dc99b285c239cc39f02f0fb92bf2656ae06faf
Jay Hong <37726672+Jay-Hong@users.noreply.github.com>
2023-09-05 18:06:15 +0900

    Headless 옵션 다양화 , 요청간격 조절 , hostname -I 추가 , range 62 -> 82
    Heasless 옵션 다양화 =  USER_AGENTS , WINDOW_SIZE , LANG 리스트 만들어 random 선택
    요청간격 조절 = time.sleep() 시간 다양하게 하여 사람이 요청하듯
    hostname -I 추가 = 실행 플랫폼 IP 확인
    range 62 -> 82 = 더 많은 정보를 가져옴

--- ildao_test_with_selenium/ildao_test_with_selenium/spiders/ildao_multi.py
+++ ildao_test_with_selenium/ildao_test_with_selenium/spiders/ildao_multi.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import re; import time;
+import re; import time; import random
 from ildao_test_with_selenium.items import IldaoTestWithSeleniumItem
 
 class IldaoMultiSpider(scrapy.Spider):
@@ -12,31 +12,80 @@ class IldaoMultiSpider(scrapy.Spider):
     allowed_domains = ["ildao.com"]
     start_urls = ["https://ildao.com/recruit"]
 
+    USER_AGENTS = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Whale/3.20.182.12 Safari/537.36',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15e148 Kakaotalk 9.5.1',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15e148 Safari/604.1',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/71.0.3578.89 Mobile/15E148 Safari/605.1',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/94.0.4606.52 Mobile/15e148 Safari/604.1',
+        'Mozilla/5.0 (Linux; Android 8.0.0; SAMSUNG-SM-G950N/KSU3CRJ1 Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/8.2 Chrome/63.0.3239.111 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 11; SM-A908N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 13; SM-S918) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 13; SM-S911) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 13; SM-S916) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 13; SM-S901) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 13; SM-S906) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 13; SM-S908) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36'
+    ]
+    
+    WINDOW_SIZES = [
+        'window-size=1920x1080',
+        'window-size=1400x800',
+        'window-size=430x932',
+        'window-size=428x926',
+        'window-size=393x852',
+        'window-size=390x844',
+        'window-size=375x812',
+        'disable-gpu'
+    ]
+
+    LANG = [
+        'lang=ko_KR',
+        'lang=en_US',
+        'lang=ko_KR',
+        'lang=ja_JP',
+        'lang=ko_KR',
+        'lang=zh-CN',
+        'lang=ko_KR'
+    ]
+
     def __init__(self):
         headlessoptions = webdriver.ChromeOptions()
         headlessoptions.add_argument('headless')
-        headlessoptions.add_argument('window-size=1920x1080')
-        headlessoptions.add_argument("disable-gpu")
-        headlessoptions.add_argument("User-Agent:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
-        headlessoptions.add_argument("lang=ko_KR")
+        headlessoptions.add_argument(random.choice(IldaoMultiSpider.LANG))
+        headlessoptions.add_argument(random.choice(IldaoMultiSpider.WINDOW_SIZES))
+        headlessoptions.add_argument(f"User-Agent: {random.choice(IldaoMultiSpider.USER_AGENTS)}")
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=headlessoptions)
         # self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     def parse(self, response):
         self.driver.get(response.url)
-        time.sleep(2)
+        time.sleep(random.randint(2, 5))
+        # time.sleep(2)
 
         # ildao_items 가져오기
         ildao_items = self.driver.find_elements(By.CSS_SELECTOR, "div.scrollsection > div.box.pointer")
 
-        for i in range(62):
+        for i in range(82):
             try:
                 print(f"목록가져오기{i} : {ildao_items[-1].location_once_scrolled_into_view}")
             except Exception as e:
                 print('\n\n - - - - - - - - 목록가져오기 예외처리 됨 !! - - - - - - - - \n\n')
-                time.sleep(2.2)
+                time.sleep(random.randint(3, 7))
+                # time.sleep(2.2)
             else:
-                time.sleep(2.2)
+                time.sleep(random.randint(3, 7))
+                # time.sleep(2.2)
                 ildao_items = self.driver.find_elements(By.CSS_SELECTOR, "div.scrollsection > div.box.pointer")
 
         # ildao_items의 '간편지원' 값을 확인하기위해 'simple_text_items'에 해당 값을 담는다
@@ -63,12 +112,14 @@ class IldaoMultiSpider(scrapy.Spider):
 
         # 간편지원 아닌 첫 index로 이동 (일반 17만원 이상 '비계/동바리'는 18만원 이상 가져오기)
         print(f"중단가기  : {ildao_items[first_no_simple].location_once_scrolled_into_view}")
-        time.sleep(2)
+        time.sleep(random.randint(2, 5))
+        # time.sleep(2)
         for index, job_item in enumerate(ildao_items):
             if index >= first_no_simple and simple_text_items[index].find('간편지원') == -1:
                 try:
                     job_item.location_once_scrolled_into_view
-                    time.sleep(.3);job_item.click();time.sleep(.3)
+                    time.sleep(random.randint(1, 4));job_item.click();time.sleep(.5)
+                    # time.sleep(.3);job_item.click();time.sleep(.3)
                     
                     title_sel = self.driver.find_element(By.CSS_SELECTOR, "#detail_info div.ft5.NotoSansM")
                     title_pre = re.sub('[^a-zA-Z0-9가-힣一-龥_\s\(\)\[\]\-\~\/\,\.\ㆍ\&]', ' ', title_sel.text)
@@ -197,15 +248,17 @@ class IldaoMultiSpider(scrapy.Spider):
                 
         '''
         # 간편지원 and 18만원 이상 완료  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-        
+        time.sleep(random.randint(5, 8))
         # 간편지원 아닌 첫 index로 다시 이동 not(일반 17만원 이상 '비계/동바리'는 18만원 이상 가져오기)
         print(f"중단가기  : {ildao_items[first_no_simple].location_once_scrolled_into_view}")
-        time.sleep(2)
+        time.sleep(random.randint(4, 7))
+        # time.sleep(2)
         for index, job_item in enumerate(ildao_items):
             if index >= first_no_simple and simple_text_items[index].find('간편지원') == -1:
                 try:
                     job_item.location_once_scrolled_into_view
-                    time.sleep(.3);job_item.click();time.sleep(.3)
+                    time.sleep(random.randint(1, 4));job_item.click();time.sleep(.5)
+                    # time.sleep(.3);job_item.click();time.sleep(.3)
 
                     title_sel = self.driver.find_element(By.CSS_SELECTOR, "#detail_info div.ft5.NotoSansM")
                     title_pre = re.sub('[^a-zA-Z0-9가-힣一-龥_\s\(\)\[\]\-\~\/\,\.\ㆍ\&]', ' ', title_sel.text)
@@ -269,7 +322,7 @@ class IldaoMultiSpider(scrapy.Spider):
                     for numpeople_sel in numpeople_sel_list:
                         if len(num_pattern.findall(numpeople_sel.text)) > 0:    # 숫자가 들어있는 문자열만 가져온다
                             numpeople_int = re.sub('[^0-9]', '', numpeople_sel.text)    # 숫자를 제외한 문자 삭제
-                            print(f"numpeople_int : {numpeople_int}")
+                            # print(f"numpeople_int : {numpeople_int}")
                             numpeople_pre += int(numpeople_int)                 # 초보+조공+준공+기공 = 총인원
                     numpeople = f"{numpeople_pre}명"
     
@@ -306,7 +359,7 @@ class IldaoMultiSpider(scrapy.Spider):
                         job_item['etc1'] = etc1
                         job_item['etc2'] = etc2
                         job_item['etc3'] = etc3
-                        print(etc_set)
+                        # print(etc_set)
                         job_item['numpeople'] = numpeople
                         job_item['phone'] = phone
                         job_item['detail'] = detail
@@ -335,7 +388,8 @@ class IldaoMultiSpider(scrapy.Spider):
                 
         '''
         # 간편지원 and 그외 완료  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
-        time.sleep(2)
+        time.sleep(random.randint(2, 22))
         print(f"\n\n\nfirst_no_simple : [{first_no_simple}]\n")# 간편지원 아닌 index 출력
         print(f"\n # # # # # # # # # # # # # # # # # # # # # #   정상종료   # # # # # # # # # # # # # # # # # # # # # #\n\n")
+        self.driver.quit()
         pass
